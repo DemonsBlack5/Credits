2fb99fb526ca9c08e3797a126c9d3ed77dea4cc4
DemonsBlack1 <44532514+DemonsBlack1@users.noreply.github.com>
2018-11-06 14:27:41 +0200

    Update Credits.js

--- Credits.js
+++ Credits.js
@@ -3,11 +3,21 @@ const client = new Discord.Client();
 console.log("ٍPotato#Army");
 
 client.on("ready", () => {
-let channel =     client.channels.get("508983436686524417")
+let channel =     client.channels.get("509339743662768138")
 setInterval(function() {
-channel.send(`DB is the Best`);
+channel.send(`
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the Best
+`);
 }, 25)
 })
  
-client.login('NTA4OTc4MzkxNzY0OTU5MjMy.DsHKcg.cQsRsFfIwUoMW8328zAPkFW_wk0');
 client.login('NTA4OTgzNTg3OTcxMDA2NDc0.DsHTGw.C7S5oxNIjTD6SDHprZdi4Ejs66U');
+client.login('NTA4OTc4MzkxNzY0OTU5MjMy.DsMX7A.TD8jcF5DgYvPKX6Dvo8mGbE8V6I');
