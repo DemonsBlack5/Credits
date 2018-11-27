09684805ef5a5801de114152ae49457c6374c617
DemonsBlack1 <44532514+DemonsBlack1@users.noreply.github.com>
2018-11-28 00:53:34 +0200

    Update Credits.js

--- Credits.js
+++ Credits.js
@@ -9,6 +9,37 @@ channel.send(`DB is the BestDB is the BestDB is the BestDB is the BestDB is the 
 
 }, 25)
 })
+
+var prefix = "^";
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// -say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
  
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+
 client.login('NTA4OTgzNTg3OTcxMDA2NDc0.DsHTGw.C7S5oxNIjTD6SDHprZdi4Ejs66U');
 
